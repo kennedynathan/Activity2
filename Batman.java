80c63b3d56c3dc69af14c33dfa5187c95aea8436
kennedynathan <146887213+kennedynathan@users.noreply.github.com>
2023-10-15 17:57:18 -0700

    Add files via upload

---
+++ Batman.java
@@ -0,0 +1,8 @@
+package app;
+
+public class Batman extends SuperHero {
+	public Batman(int health) {
+		super("Batman", health);
+	}
+	
+}
---
+++ Weapon.java
@@ -0,0 +1,8 @@
+package app;
+
+public abstract class Weapon {
+	public void fireWeapon(int power) {
+		System.out.println("In Weapon.fireWeapon() with a power of "+ power);
+	}
+	public abstract void activate(boolean enable);
+}
